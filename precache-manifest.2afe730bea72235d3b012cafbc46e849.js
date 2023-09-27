bb30109fe98acfb9b1d739191c829dd46e47cdd4
Miguel Péres <foxmpc@gmail.com>
2023-09-27 20:39:21 -0300

    Updates

---
+++ precache-manifest.2afe730bea72235d3b012cafbc46e849.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ecc006be236281c317f",
+    "url": "/beer-around-the-world/static/css/main.49d4ad25.chunk.css"
+  },
+  {
+    "revision": "6ecc006be236281c317f",
+    "url": "/beer-around-the-world/static/js/main.5504a079.chunk.js"
+  },
+  {
+    "revision": "972dd3e22452280d40cd",
+    "url": "/beer-around-the-world/static/js/runtime~main.79da9fe4.js"
+  },
+  {
+    "revision": "fbaa8beb37b656f9a197",
+    "url": "/beer-around-the-world/static/css/2.9cecdfb2.chunk.css"
+  },
+  {
+    "revision": "fbaa8beb37b656f9a197",
+    "url": "/beer-around-the-world/static/js/2.e2337f23.chunk.js"
+  },
+  {
+    "revision": "d89ce7acc223c426e0e37d3bff2a0a5e",
+    "url": "/beer-around-the-world/index.html"
+  }
+];
